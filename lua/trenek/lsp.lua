e05abc9e56ee2c8446bec9d2037ce0bc4b6e862f
Trenek <mateusz.sas@vp.pl>
2026-01-12 00:08:19 +0100

    Updated LSP Related Mappings

--- lua/trenek/lsp.lua
+++ lua/trenek/lsp.lua
@@ -20,27 +20,27 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local opts = { buffer = event.buf }
         local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
 
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
 
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'grr', '<cmd>lua vim.lsp.buf.document_highlight()<cr>', opts)
-        vim.keymap.set('n', '<ESC>', '<cmd>lua vim.lsp.buf.clear_references()<cr>', opts)
+        vim.keymap.set('n', 'gk', vim.lsp.buf.document_highlight, opts)
+        vim.keymap.set('n', '<ESC>', vim.lsp.buf.clear_references, opts)
 
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        -- gra - code actions
+        -- grn - rename
+        -- grr - references
+        -- Ctr+S in insert mode = signature_help
+        vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
         vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
-        vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.setqflist()<cr>', opts)
-        vim.keymap.set('n', '<leader>ee', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-        vim.keymap.set('n', '<leader>es', function() diagnosticConfig(true) end)
-        vim.keymap.set('n', '<leader>ef', function() diagnosticConfig(false) end)
-        vim.keymap.set('n', '<leader>en', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
-        vim.keymap.set('n', '<leader>ep', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+        -- use ]d, [d, [D. ]D to jump between diagnostics
+        vim.keymap.set('n', '<leader>d', vim.diagnostic.setqflist, opts)
+        vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '<leader>ds', function() diagnosticConfig(true) end)
+        vim.keymap.set('n', '<leader>df', function() diagnosticConfig(false) end)
 
         if client:supports_method('textDocument/completion') then
             client.server_capabilities.completionProvider.triggerCharacters = chars
